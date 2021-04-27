45b7eba1c13ebca14ca64edc7d47ebb4e2801275
edwin <ecolladocastillo@gmail.com>
2021-04-26 20:58:37 -0500

    anadir las tablas a al base de datos y el campo dirreccion en users

---
+++ database/migrations/2021_04_27_002602_create_productos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre_p');
+            $table->integer('precio_p');
+            $table->string('descripcion_p');
+            $table->string('imagen_p');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
